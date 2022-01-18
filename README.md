b79ec5389675051f05f46c73f0db9750c488ca1a
Yelloooowww <cococing0324@gmail.com>
2022-01-18 10:24:52 +0800

    door detector training

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+# Vehicular Vision System Final report - Door Detector
+![](image/FCN.png)
+
+[dataset](https://drive.google.com/file/d/1ZMPC3CnlHRTmmfyeaH7G8GxcT60AcDgi/view?usp=sharing)
+
+[weight](https://drive.google.com/file/d/105TI8N8b042_xvk49hJbGaZiSoNHaGi4/view?usp=sharing)
+
+## Training
+`python3 train.py`
+
+
 # Fully Convolutional Networks
 [Tutorial](https://drive.google.com/drive/u/1/folders/1P6asV3aCKYl8luQsDa3Y2gQXgmr9ViNu)
---
+++ train.py
@@ -0,0 +1,378 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import models
+from torchvision.models.vgg import VGG
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import scipy.misc
+import random
+import sys
+import cv2
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+from matplotlib import pyplot as plt
+import numpy as np
+import time
+import os
+import logging
+
+class FCN16s(nn.Module):
+
+    def __init__(self, pretrained_net, n_class):
+        super(FCN16s, self).__init__()
+        self.n_class = n_class
+        self.pretrained_net = pretrained_net
+        self.relu    = nn.ReLU(inplace = True)
+        self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn1     = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn2     = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn3     = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn5     = nn.BatchNorm2d(32)
+        self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
+
+    def forward(self, x):
+        output = self.pretrained_net(x)
+
+        # After the feature extraction layer of vgg, you can get the feature map.
+        # The size of the feature map after 5 max_pools are respectively
+        x5 = output['x5']  # size=(N, 512, x.H/32, x.W/32)
+        x4 = output['x4']  # size=(N, 512, x.H/16, x.W/16)
+        x3 = output['x3']  # size=(N, 256, x.H/8,  x.W/8)
+        x2 = output['x2']  # size=(N, 128, x.H/4,  x.W/4)
+        x1 = output['x1']  # size=(N, 64, x.H/2,  x.W/2)
+
+#===========FCN16s model ==========================
+        score = self.relu(self.deconv1(x5))               # size=(N, 512, x.H/16, x.W/16)
+        score = self.bn1(score + x4)                      # element-wise add, size=(N, 512, x.H/16, x.W/16)
+        score = self.bn2(self.relu(self.deconv2(score)))  # size=(N, 256, x.H/8, x.W/8)
+        score = self.bn3(self.relu(self.deconv3(score)))  # size=(N, 128, x.H/4, x.W/4)
+        score = self.bn4(self.relu(self.deconv4(score)))  # size=(N, 64, x.H/2, x.W/2)
+        score = self.bn5(self.relu(self.deconv5(score)))  # size=(N, 32, x.H, x.W)
+        score = self.classifier(score)                    # size=(N, n_class, x.H/1, x.W/1)
+#===========FCN16s model ==========================
+
+#===========Please design a FCN8s model ===========
+#         score = self.relu(self.deconv1(x5))               # size=(N, 512, x.H/16, x.W/16)
+#         score = self.bn1(score + x4)                      # element-wise add, size=(N, 512, x.H/16, x.W/16)
+#         score = self.relu(self.deconv2(score))            # size=(N, 256, x.H/8, x.W/8)
+#         score = self.bn2(score + x3)                      # element-wise add, size=(N, 256, x.H/8, x.W/8)
+#         score = self.bn3(self.relu(self.deconv3(score)))  # size=(N, 128, x.H/4, x.W/4)
+#         score = self.bn4(self.relu(self.deconv4(score)))  # size=(N, 64, x.H/2, x.W/2)
+#         score = self.bn5(self.relu(self.deconv5(score)))  # size=(N, 32, x.H, x.W)
+#         score = self.classifier(score)
+
+#===========Please design a FCN8s model ===========
+
+
+        return score  # size=(N, n_class, x.H/1, x.W/1)
+
+
+
+
+class VGGNet(VGG):
+    def __init__(self, pretrained=True, model='vgg16', requires_grad=True, remove_fc=True, show_params=False):
+        super(VGGNet, self).__init__(make_layers(cfg[model]))
+        self.ranges = ranges[model]
+
+        if pretrained:
+            exec("self.load_state_dict(models.%s(pretrained=True).state_dict())" % model)
+
+        if not requires_grad:
+            for param in super().parameters():
+                param.requires_grad = False
+
+        if remove_fc:  # delete redundant fully-connected layer params, can save memory
+            del self.classifier
+
+        if show_params:
+            for name, param in self.named_parameters():
+                print(name, param.size())
+
+    def forward(self, x):
+        output = {}
+
+        # get the output of each maxpooling layer (5 maxpool in VGG net)
+        for idx in range(len(self.ranges)):
+            for layer in range(self.ranges[idx][0], self.ranges[idx][1]):
+                x = self.features[layer](x)
+            output["x%d"%(idx+1)] = x
+        return output
+
+
+
+ranges = {
+    'vgg11': ((0, 3), (3, 6),  (6, 11),  (11, 16), (16, 21)),
+    'vgg13': ((0, 5), (5, 10), (10, 15), (15, 20), (20, 25)),
+    'vgg16': ((0, 5), (5, 10), (10, 17), (17, 24), (24, 31)),
+    'vgg19': ((0, 5), (5, 10), (10, 19), (19, 28), (28, 37))
+}
+
+# cropped version from https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+cfg = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+batch_size = 8 #3
+epochs     = 500
+lr         = 1e-4
+momentum   = 0
+w_decay    = 1e-5
+step_size  = 50
+gamma      = 0.5
+model_use  = "door" # "subt_model"
+n_class = 2
+
+
+# get data
+FullPath = os.getcwd()
+data_dir  = os.path.join(FullPath + "/data/door")
+if not os.path.exists(data_dir):
+    print("Data not found!")
+
+# create dir for model
+model_dir = os.path.join(FullPath + "/models", model_use)
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
+
+# create dir for score
+score_dir = os.path.join(FullPath + "/scores", model_use)
+if not os.path.exists(score_dir):
+    os.makedirs(score_dir)
+
+use_gpu = torch.cuda.is_available()
+num_gpu = list(range(torch.cuda.device_count()))
+
+vgg_model = VGGNet(requires_grad=True, remove_fc=True)
+fcn_model = FCN16s(pretrained_net=vgg_model, n_class=n_class)
+
+if use_gpu:
+    ts = time.time()
+    vgg_model = vgg_model.cuda()
+    fcn_model = fcn_model.cuda()
+    fcn_model = nn.DataParallel(fcn_model, device_ids=num_gpu)
+    print("Finish cuda loading, time elapsed {}".format(time.time() - ts))
+else:
+#     nn.DataParallel(fcn_model)
+    print("Use CPU to train.")
+
+print(fcn_model)
+params = list(fcn_model.parameters())
+
+
+
+
+
+
+
+means     = np.array([103.939, 116.779, 123.68]) / 255. # mean of three channels in the order of BGR
+h, w      = 320, 640
+val_h     = h
+val_w     = w
+
+class product_dataset(Dataset):
+
+    def __init__(self, root, phase, n_class=n_class, flip_rate=0.):
+        data_dir = os.path.join(root, phase)
+        self.rgb_list = os.listdir(os.path.join(data_dir, 'images'))
+        _list = self.rgb_list
+        self.label_list = []
+        for i in range(len(self.rgb_list)):
+            self.label_list.append(_list[i].split(".")[0] + ".png")
+
+        self.rgb_dir = os.path.join(data_dir, 'images')
+        self.label_dir = os.path.join(data_dir, 'masks')
+        self.means     = means
+        self.n_class   = n_class
+        self.flip_rate = flip_rate
+        if phase == 'train':
+            self.flip_rate = 0.5
+
+    def __len__(self):
+        return len(self.rgb_list)
+
+    def __getitem__(self, idx):
+        idx = idx % len(self.rgb_list)
+        img = cv2.imread(os.path.join(self.rgb_dir, self.rgb_list[idx]),cv2.IMREAD_UNCHANGED)
+        label = cv2.imread(os.path.join(self.label_dir, self.label_list[idx]), cv2.IMREAD_GRAYSCALE)
+
+        label[label == 38] = 1 # door
+
+        img = cv2.resize(img, (640, 320), interpolation=cv2.INTER_CUBIC)
+        label = cv2.resize(label, (640, 320), interpolation=cv2.INTER_CUBIC)
+
+        origin_img = img
+        if random.random() < self.flip_rate:
+            img   = np.fliplr(img)
+            label = np.fliplr(label)
+
+        # reduce mean
+        img = img[:, :, ::-1]  # switch to BGR
+
+        img = np.transpose(img, (2, 0, 1)) / 255.
+        img[0] -= self.means[0]
+        img[1] -= self.means[1]
+        img[2] -= self.means[2]
+
+        # convert to tensor
+        img = torch.from_numpy(img.copy()).float()
+        label = torch.from_numpy(label.copy()).long()
+
+        # create one-hot encoding
+        h, w = label.size()
+        target = torch.zeros(self.n_class, h, w)
+
+        for i in range(n_class):
+            target[i][label == i] = 1
+
+#         target[0][label == 0] = 1
+#         print(np.unique(label))
+
+
+        sample = {'X': img, 'Y': target, 'l': label, 'origin': origin_img}
+
+        return sample
+
+
+
+
+
+
+# initial dataloader for trainning and validation
+train_data = product_dataset(data_dir, phase = 'train')
+val_data   = product_dataset(data_dir, phase = 'train')
+dataloader = DataLoader(train_data, batch_size = batch_size, shuffle=True, num_workers = 0)
+val_loader = DataLoader(val_data, batch_size = 1,  shuffle=True, num_workers = 0)
+
+dataiter = iter(dataloader)
+
+# define loss function
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.RMSprop(fcn_model.parameters(), lr = lr, momentum = momentum, weight_decay = w_decay)
+# decay LR by a factor of 0.5 every step_size = 50 epochs
+scheduler = lr_scheduler.StepLR(optimizer, step_size = step_size, gamma = gamma)
+
+
+
+loss_list = []
+def train():
+    for epoch in range(epochs):
+        fcn_model.train()
+        scheduler.step()
+        configs    = "FCNs_{}_batch{}_epoch{}_RMSprop_lr{}"\
+            .format(model_use, batch_size, epoch, lr)
+        model_path = os.path.join(model_dir, configs)
+
+        ts = time.time()
+        for iter, batch in enumerate(dataloader):
+            optimizer.zero_grad()
+
+            if use_gpu:
+                inputs = Variable(batch['X'].cuda())
+                labels = Variable(batch['Y'].cuda())
+            else:
+                inputs, labels = Variable(batch['X']), Variable(batch['Y'])
+
+            outputs = fcn_model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            if iter % 1 == 0:
+                print("epoch{}, iter{}, loss: {}".format(epoch+1, iter, loss.item()))
+
+            loss_list.append(loss.item())
+
+        print("Finish epoch {}, time elapsed {}".format(epoch+1, time.time() - ts))
+        if epoch % 1 == 0:
+            torch.save(fcn_model.state_dict(),model_path + '.pkl')
+            plt.plot(loss_list)
+            plt.xlabel('iteration')
+            plt.ylabel('loss')
+            plt.title('FCN Traning Loss')
+            plt.savefig("loss.png")
+
+
+
+
+
+def prediction(model_name):
+
+    # load pretrain models
+
+    vgg_model = VGGNet(requires_grad=True, remove_fc=True)
+    fcn_model = FCN16s(pretrained_net=vgg_model, n_class=n_class)
+    fcn_model = nn.DataParallel(fcn_model)
+
+    state_dict = torch.load(os.path.join(model_dir, model_name), map_location='cpu')
+    fcn_model.load_state_dict(state_dict)
+
+    batch = dataiter.next()
+    if use_gpu:
+        inputs = Variable(batch['X'].cuda())
+    else:
+        inputs = Variable(batch['X'])
+    img    = batch['origin']
+    label  = batch['l']
+
+    inputs = Variable(batch['X'])
+    output = fcn_model(inputs)
+    output = output.data.cpu().numpy()
+
+    N, _, h, w = output.shape
+    pred = output.transpose(0, 2, 3, 1).reshape(-1, n_class).argmax(axis = 1).reshape(N, h, w)
+
+
+    # show images
+    plt.figure(figsize = (10, 12))
+    img = img.numpy()
+    for i in range(N):
+        img[i] = cv2.cvtColor(img[i], cv2.COLOR_BGR2RGB)
+        plt.subplot(N, 3, i*3 + 1)
+        plt.title("origin_img")
+        plt.imshow(img[i])
+        #print(np.unique(_img[i]))
+
+        plt.subplot(N, 3, i*3 + 2)
+        plt.title("label_img")
+        plt.imshow(label[i],cmap = "nipy_spectral",vmin = 0, vmax = n_class - 1)
+
+        plt.subplot(N, 3, i*3 + 3)
+        plt.title("prediction")
+        plt.imshow(pred[i],cmap = "nipy_spectral",vmin = 0, vmax = n_class - 1)
+
+    plt.show()
+
+
+
+
+train()
+
+
+# prediction("FCNs_door_batch8_epoch44_RMSprop_lr0.0001.pkl")
